82339a2d1b9045bf792c9a9c234e8bcd59b51a15
SayaDimas <dimasrifkinuramadani@gmail.com>
2025-01-02 16:27:59 +0700

    lagi dan lagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 <div class="container px-4 px-lg-5">
                     <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
                         <div class="col-xl-8 col-lg-7" data-aos="fade-right">
-                            <img class="img-fluid mb-3 mb-lg-0" src="{{ asset('img/kecelakaan.png') }}" alt="..." />
+                            <img class="img-fluid mb-3 mb-lg-0" src="{{ asset('img/bersihinkapal.jpeg') }}" alt="..." />
                         </div>
                         <div class="col-xl-4 col-lg-5" data-aos="fade-left">
                             <div class="featured-text text-center text-lg-left">
